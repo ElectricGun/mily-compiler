13b030c662b15ad5cfb46fd11e118e396339934e
ElectricGun <68138799+ElectricGun@users.noreply.github.com>
2025-07-20 08:08:44 +0700

    removed unnecessary method

--- src/tokens/Token.java
+++ src/tokens/Token.java
@@ -19,10 +19,6 @@ public class Token {
         return string.length();
     }
 
-    public char charAt(int i) {
-        return string.charAt(i);
-    }
-
     @Override
     public String toString() {
         return string;
