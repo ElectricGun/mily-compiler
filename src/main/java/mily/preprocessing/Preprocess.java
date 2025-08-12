b5305000ad9294d3a88f09f298fa5d24ba6bb0a1
me <68138799+ElectricGun@users.noreply.github.com>
2025-08-12 13:52:01 +0700

    removed $ in #include

--- src/main/java/mily/preprocessing/Preprocess.java
+++ src/main/java/mily/preprocessing/Preprocess.java
@@ -18,49 +18,35 @@ public class Preprocess {
             Token token = tokenList.remove(0);
 
             if (token.equalsKey(KEY_HASH)) {
-//                Token macroKeyword = tokenList.removeFirst();
                 Token macroKeyword = tokenList.remove(0);
 
                 while (Functions.isWhiteSpace(macroKeyword))
-//                    macroKeyword = tokenList.removeFirst();
                     macroKeyword = tokenList.remove(0);
 
                 if (macroKeyword.equalsKey(KEY_INCLUDE)) {
-//                    Token macroStart = tokenList.removeFirst();
-                    Token macroStart = tokenList.remove(0);
-                    while (Functions.isWhiteSpace(macroStart))
-//                        macroStart = tokenList.removeFirst();
-                        macroStart = tokenList.remove(0);
+                    StringBuilder buffer = new StringBuilder();
 
-                    if (macroStart.equalsKey(KEY_MACRO_LITERAL)) {
-                        StringBuilder buffer = new StringBuilder();
+                    Token currMacroToken;
+                    while (true) {
+                        currMacroToken = tokenList.remove(0);
 
-                        Token currMacroToken;
-                        while (true) {
-//                            currMacroToken = tokenList.removeFirst();
-                            currMacroToken = tokenList.remove(0);
+                        if (currMacroToken.equalsKey(KEY_SEMICOLON)) {
+                            break;
 
-                            if (currMacroToken.equalsKey(KEY_SEMICOLON)) {
-                                break;
-
-                            } else {
-                                if (tokenList.isEmpty()) {
-                                    throw new Exception("Unclosed macro from line " + macroStart.line);
-                                }
-                                buffer.append(currMacroToken.string);
+                        } else {
+                            if (tokenList.isEmpty()) {
+                                throw new Exception(String.format("Unclosed \"%s\" statement", KEY_INCLUDE) + token.line);
                             }
+                            buffer.append(currMacroToken.string);
                         }
-                        File includedFile = new File(directory, buffer.toString().trim());
-                        CodeFile includedCode = Functions.readFile(includedFile.getParent(), includedFile.getName());
-
-                        List<Token> includedTokens = Lexing.tokenize(includedCode.getCode(), includedFile.getPath(), debugMode);
-                        includedTokens = processIncludes(includedTokens, includedFile.getParent(), debugMode);
-
-                        newTokenList.addAll(includedTokens);
-
-                    } else {
-                        throw new Exception("Expecting macro, found \"" + macroKeyword + "\" instead");
                     }
+                    File includedFile = new File(directory, buffer.toString().trim());
+                    CodeFile includedCode = Functions.readFile(includedFile.getParent(), includedFile.getName());
+
+                    List<Token> includedTokens = Lexing.tokenize(includedCode.getCode(), includedFile.getPath(), debugMode);
+                    includedTokens = processIncludes(includedTokens, includedFile.getParent(), debugMode);
+
+                    newTokenList.addAll(includedTokens);
 
                 } else {
                     throw new Exception("Unknown macro keyword \"" + macroKeyword + "\"");
